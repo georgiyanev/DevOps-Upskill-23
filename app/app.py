ce976216d26bda6b7ec123ef4e8ca031edd3f2d3
@georgi.yanev <georgi.yanev@economedia.bg>
2023-12-12 09:46:21 +0200

    Added small Kubernetes deployment scenario

--- app/app.py
+++ app/app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return "Hello, Kubernetes!"
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=os.environ.get("PORT", 80))
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get("PORT", 5000))
