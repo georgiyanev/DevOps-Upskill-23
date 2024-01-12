4ace0d7fa741fc58c14cafed5197b9982351ee30
@georgi.yanev <georgi.yanev@economedia.bg>
2024-01-12 15:56:36 +0200

    Test deploy

--- app/app.py
+++ app/app.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, Kubernetes!"
+    return "Test deploy!"
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=os.environ.get("PORT", 80))
