54cd62f38313aab30fbb3e6443606e0e1a80aaf5
@georgi.yanev <georgi.yanev@economedia.bg>
2024-01-27 10:58:00 +0200

    Test builded Docker image with Trivy

--- app/app.py
+++ app/app.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Test deploy: 27.01 - Trivy!"
+    return "Test deploy - Trivy!!!!"
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=os.environ.get("PORT", 80))
--- app/app_test.py
+++ app/app_test.py
@@ -10,6 +10,6 @@ class TestApp(unittest.TestCase):
     def test_hello_world(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b"Test deploy: 27.01 - Trivy!")
+        self.assertEqual(response.data, b"Test deploy - Trivy!!!!")
 if __name__ == "__main__":
     unittest.main()
