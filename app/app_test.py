1ca3b5efa71a34ab91a52d04a86bf30612418b39
@georgi.yanev <georgi.yanev@economedia.bg>
2024-01-27 12:06:24 +0200

    Push Docker image only if changes are in the app folder

--- app/app_test.py
+++ app/app_test.py
@@ -10,6 +10,6 @@ class TestApp(unittest.TestCase):
     def test_hello_world(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b"Test deploy - App folder 3!")
+        self.assertEqual(response.data, b"Test deploy - 27.01/v.0.5!")
 if __name__ == "__main__":
     unittest.main()
