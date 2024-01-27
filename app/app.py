7ef87067c63ca71ffe175bfb06cad2a28e7f0995
@georgi.yanev <georgi.yanev@economedia.bg>
2024-01-27 11:06:34 +0200

    Push Docker image only of changes are in the app folder

--- app/app.py
+++ app/app.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Test deploy - Trivy!!!!"
+    return "Test deploy - Github actions!"
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=os.environ.get("PORT", 80))
