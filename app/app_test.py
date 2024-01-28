133e4425a40346f413d94bb13bb68e11be4c4bbe
@georgi.yanev <georgi.yanev@economedia.bg>
2024-01-28 10:28:44 +0200

    Add small fixes and push to trigger the pipeline

--- app/app_test.py
+++ app/app_test.py
@@ -10,6 +10,6 @@ class TestApp(unittest.TestCase):
     def test_hello_world(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b"Test deploy - 27.01/v.0.5!")
+        self.assertEqual(response.data, b"Test deploy - 28.01/v.0.1!")
 if __name__ == "__main__":
     unittest.main()
